e5c0bfff0a5f6acb00c1c8b66e05b552bb988fa9
Дмитрий <skokovdmitry@bk.ru>
2025-06-09 18:10:45 +0300

    5

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 from rest_framework.validators import UniqueTogetherValidator
-from rest_framework.serializers import CurrentUserDefault
+
 
 from posts.models import Comment, Post, Group, Follow, User
 
